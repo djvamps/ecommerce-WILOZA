eba02f773fefa7a4801783a5372466eb1c4c039a
djvamps <dalopeza9@hotmail.com>
2026-08-31 18:35:32 -0500

    fix: agregar tipo explicit SiteConfig

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,25 @@
-export type SiteConfig = typeof siteConfig
+export interface SiteConfig {
+  name: string
+  description: string
+  url: string
+  ogImage: string
+  mainNav: {
+    title: string
+    href: string
+  }[]
+  links: {
+    x: string
+    github: string
+    docs: string
+    login: string
+  }
+}
 
-export const siteConfig = {
+export const siteConfig: SiteConfig = {
   name: "WILOZA",
   description: "Tienda de ropa oversize y streetwear para desarrolladores.",
+  url: "https://wiloza.vercel.app", // Cambia por tu dominio real
+  ogImage: "https://wiloza.vercel.app/wiloza_fanpage.png", // Ruta a tu imagen de vista previa
   mainNav: [
     {
       title: "Catálogo",
